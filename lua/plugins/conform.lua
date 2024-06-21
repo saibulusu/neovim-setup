8117e47ab8f7be040dd275a8723cd777db6dac43
saibulusu <sai.bulusu@gmail.com>
2024-06-20 22:00:09 -0700

    gitignore file

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -4,7 +4,6 @@ return {
   config = function()
     require("conform").setup({
       format_on_save = {
-        -- These options will be passed to conform.format()
         timeout_ms = 500,
         lsp_fallback = true,
       }
