ce1a95280fddec9d613e087d6f25744a182c305f
saibulusu <sai.bulusu@gmail.com>
2024-06-21 22:28:18 -0700

    More gitsigns configurations.

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -5,6 +5,7 @@ return {
     vim.keymap.set('n', '<leader>hp', ':Gitsigns preview_hunk<CR>', {})
     vim.keymap.set('n', '<leader>tb', ':Gitsigns toggle_current_line_blame<CR>', {})
     vim.keymap.set('n', '<leader>hs', ':Gitsigns stage_hunk', {})
+    vim.keymap.set('n', '<leader>hb', ':Gitsigns stage_buffer', {})
     vim.keymap.set('n', '<leader>hu', ':Gitsigns undo_stage_hunk', {})
     vim.keymap.set('n', '<leader>hr', ':Gitsigns reset_hunk', {})
   end
