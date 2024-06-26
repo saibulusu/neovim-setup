b64c8f257485331ef100740131cc2c7ea4a08a4f
saibulusu <sai.bulusu@gmail.com>
2024-06-26 11:30:26 -0700

    Allowing C-n & <leader>-n both for neo tree keympa

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -8,5 +8,6 @@ return {
   },
   config = function()
     vim.keymap.set('n', '<leader>n', ':Neotree filesystem reveal left<CR>', {})
+    vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
   end,
 }
